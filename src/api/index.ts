663c2af17637c2a1f4dd7b822b07033333d93b49
Ryan Feng <ryan.feng@bondexchange.com.au>
2024-03-16 23:33:29 +1100

    Created a api call func

---
+++ src/api/index.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { Category } from '../store/categories/category.types';
+
+async function getCategoriesFromAPI(): Promise<{ shop_data: Category[] }> {
+
+    let url;
+
+    if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") {
+        url = 'http://localhost:3001/shop_data';
+    } else {
+        url = 'https://raw.githubusercontent.com/ryanskystack/ecom/master/assets/shop-data.json';
+    }
+
+    const response = await axios.get(url);
+    const data = response.data;
+    console.log('data', data);
+    return { shop_data: data };
+}
+
+export default getCategoriesFromAPI;
